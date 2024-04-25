4d97c738a3b180769d30826c4974d4af34faf53b
Adi Nugroho <adi.nugroho@finnet.co.id>
2024-04-25 21:29:58 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,167 @@
 import Image from "next/image";
+import {Open_Sans} from "next/font/google";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+import sofaL from "@/assets/images/Sofa L.png";
+import logo from "@/assets/images/logo-02 1.png";
+import sofaRetro from "@/assets/images/Sofa Retro 1.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="flex min-h-screen w-full flex-col items-center justify-between">
+        <div className="flex w-full h-screen relative">
+            <div className="flex w-full h-screen">
+                <div className="w-2/5 flex flex-col">
+                <Image
+                    src={logo.src}
+                    alt="Logo Difa"
+                    width={logo.width}
+                    height={logo.height}
+                    className="mx-auto"
+                />
+                </div>
+                <div className="h-3/5 w-3/5 bg-blue-900 rounded-bl-[10rem]">
+                    <ul className="flex justify-evenly text-white py-3">
+                        <li>Product</li>
+                        <li>Our Store</li>
+                        <li>FAQ</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="h-3/6 w-2/6 bg-white bg-opacity-70 top-40 left-56 absolute rounded-2xl p-9 flex flex-col">
+                <h1 className={`text-4xl font-semibold whitespace-pre-line ${openSans.className}`}>TEMUKAN FURNITURE IDAMAN ANDA</h1>
+                <h3 className="py-3 pr-12 flex-grow">Ekspresikan ruangan anda dengan furnitur sesuai kebutuhan bersama Difa Sejahtera Mebel</h3>
+                <div className="flex justify-evenly">
+                    <select name="" id="" className="w-full h-8 border rounded-md">
+                        <option value="">A</option>
+                        <option value="">B</option>
+                        <option value="">C</option>
+                        <option value="">D</option>
+                    </select>
+                    <button className="bg-blue-900 flex-grow ml-3 w-64 rounded-md text-white">CARI FURNITURE</button>
+                </div>
+            </div>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            src={sofaL.src}
+            alt="Sofa L"
+            width={sofaL.width}
+            height={sofaL.height}
+            className="absolute right-20 top-56"            
             />
-          </a>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="w-full h-full bg-gray-300 py-12 px-24 flex">
+            <div className="w-64 h-fit p-6 mr-12 my-auto bg-blue-900 text-white rounded-md ">
+                <h1 className={`text-xl font-semibold whitespace-pre-line ${openSans.className}`}>BEST FAVORITE FURNITURE</h1>
+                <p className={`text-sm whitespace-pre-line pt-3 ${openSans.className}`}>Rekomendasi furniture untuk ruangan anda </p>
+                <div className="flex justify-end pt-6">
+                    <button className="bg-yellow-500 py-3 px-6 rounded-full">Cari</button>
+                </div>
+            </div>
+            <div className="w-full">
+                <div className="w-56 h-80 bg-white rounded-2xl mr-6 relative overflow-visible inline-block">
+                    <div className="h-1/2 bg-gray-100 rounded-t-2xl overflow-hidden relative">
+                        <Image
+                            src={sofaRetro.src}
+                            alt="item 1"
+                            layout="fill"
+                            objectFit="cover"
+                            className="bg-gray-100"
+                            
+                        />
+                    </div>
+                    <div className="h-1/2 p-3 flex flex-col text-center justify-evenly">
+                        <h1 className="text-xl font-semibold">Sofa Retro</h1>
+                        <h1 className="text-sm line-through decoration-red-500">IDR 2.000.000</h1>
+                        <h1 className="text-base">IDR 1.500.000</h1>
+                    </div>
+                    <div className="h-11 w-28 bg-blue-700 absolute top-[-10px] right-[-10px] rounded-tr-lg rounded-bl-[35px] text-center flex flex-col justify-center">
+                        <div className="text-white italic text-sm">
+                            Disc 20%
+                        </div>
+                    </div>
+                    <div className=""></div>
+                </div>
+                <div className="w-56 h-80 bg-white rounded-2xl mr-6 relative overflow-visible inline-block">
+                    <div className="h-1/2 bg-gray-100 rounded-t-2xl overflow-hidden relative">
+                        <Image
+                            src={sofaRetro.src}
+                            alt="item 1"
+                            layout="fill"
+                            objectFit="cover"
+                            className="bg-gray-100"
+                            
+                        />
+                    </div>
+                    <div className="h-1/2 p-3 flex flex-col text-center justify-evenly">
+                        <h1 className="text-xl font-semibold">Sofa Retro</h1>
+                        <h1 className="text-sm line-through decoration-red-500">IDR 2.000.000</h1>
+                        <h1 className="text-base">IDR 1.500.000</h1>
+                    </div>
+                    <div className="h-11 w-28 bg-blue-700 absolute top-[-10px] right-[-10px] rounded-tr-lg rounded-bl-[35px] text-center flex flex-col justify-center">
+                        <div className="text-white italic text-sm">
+                            Disc 20%
+                        </div>
+                    </div>
+                    <div className=""></div>
+                </div>
+                <div className="w-56 h-80 bg-white rounded-2xl mr-6 relative overflow-visible inline-block">
+                    <div className="h-1/2 bg-gray-100 rounded-t-2xl overflow-hidden relative">
+                        <Image
+                            src={sofaRetro.src}
+                            alt="item 1"
+                            layout="fill"
+                            objectFit="cover"
+                            className="bg-gray-100"
+                            
+                        />
+                    </div>
+                    <div className="h-1/2 p-3 flex flex-col text-center justify-evenly">
+                        <h1 className="text-xl font-semibold">Sofa Retro</h1>
+                        <h1 className="text-sm line-through decoration-red-500">IDR 2.000.000</h1>
+                        <h1 className="text-base">IDR 1.500.000</h1>
+                    </div>
+                    <div className="h-11 w-28 bg-blue-700 absolute top-[-10px] right-[-10px] rounded-tr-lg rounded-bl-[35px] text-center flex flex-col justify-center">
+                        <div className="text-white italic text-sm">
+                            Disc 20%
+                        </div>
+                    </div>
+                    <div className=""></div>
+                </div>
+                <div className="w-56 h-80 bg-white rounded-2xl mr-6 relative overflow-visible inline-block">
+                    <div className="h-1/2 bg-gray-100 rounded-t-2xl overflow-hidden relative">
+                        <Image
+                            src={sofaRetro.src}
+                            alt="item 1"
+                            layout="fill"
+                            objectFit="cover"
+                            className="bg-gray-100"
+                            
+                        />
+                    </div>
+                    <div className="h-1/2 p-3 flex flex-col text-center justify-evenly">
+                        <h1 className="text-xl font-semibold">Sofa Retro</h1>
+                        <h1 className="text-sm line-through decoration-red-500">IDR 2.000.000</h1>
+                        <h1 className="text-base">IDR 1.500.000</h1>
+                    </div>
+                    <div className="h-11 w-28 bg-blue-700 absolute top-[-10px] right-[-10px] rounded-tr-lg rounded-bl-[35px] text-center flex flex-col justify-center">
+                        <div className="text-white italic text-sm">
+                            Disc 20%
+                        </div>
+                    </div>
+                    <div className=""></div>
+                </div>
+                
+            </div>
+        </div>
+        <div className="w-full flex flex-col items-center">
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+            <div>ad</div>
+        </div>
     </main>
   );
 }
